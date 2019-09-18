d64ff22beedef8adccf7a2139c2520e08964e59d
aoba <ipbdipbd00@gmail.com>
2019-09-18 22:05:16 +0900

    Create config file

---
+++ src/config/index.ts
@@ -0,0 +1,39 @@
+// TODO:
+// process.envの返り値の型をnode.d.tsでオーバーライドしたのでもっと簡単にかける
+
+const commonConfig = {
+  env: process.env.NODE_ENV || 'development',
+  isDev: process.env.NODE_ENV !== 'production',
+  basename: process.env.PUBLIC_PATH,
+  isBrowser: typeof window !== 'undefined',
+  apiUrl: '',
+};
+
+const developmentConfig = {
+  apiUrl: 'http://localhost:3000',
+};
+
+const productionConfig = {
+  apiUrl: 'https://enfee',
+};
+
+const testConfig = {};
+
+type IEnv = 'development' | 'production' | 'test';
+
+type IEnvConfig =
+  | typeof developmentConfig
+  | typeof productionConfig
+  | typeof testConfig;
+
+const getEnvConfig = (env: IEnv): IEnvConfig => {
+  if (env === 'development') return developmentConfig;
+  if (env === 'production') return productionConfig;
+  if (env === 'test') return testConfig;
+
+  return developmentConfig;
+};
+
+const envConfig = getEnvConfig(process.env.NODE_ENV || 'development');
+
+export default { ...commonConfig, ...envConfig };
