34c9ec5ea4545c2bc85ca83098ae83f5a6e92351
aoba <ipbdipbd00@gmail.com>
2019-09-24 04:00:46 +0900

    Fix type of props passed from rating's storybook

--- src/components/atoms/Rating/index.stories.tsx
+++ src/components/atoms/Rating/index.stories.tsx
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react';
 import Rating from '.';
 
 storiesOf('atoms/Rating', module)
-  .add('rate2.5', () => <Rating rating="5" height={20} />)
-  .add('rate5.0', () => <Rating rating="8" height={20} />);
+  .add('rate2.5', () => <Rating rating={5} height={20} />)
+  .add('rate5.0', () => <Rating rating={8} height={20} />);
