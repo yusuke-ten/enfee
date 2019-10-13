1928c82aa39da1ff6bbb272c1824de277e000cf5
aoba <ipbdipbd00@gmail.com>
2019-10-14 05:40:56 +0900

    Fix shallow nest of validation test

--- src/__tests__/services/validation/index.test.ts
+++ src/__tests__/services/validation/index.test.ts
@@ -2,34 +2,28 @@ import { validationEmail, validationPassword } from 'services/validation';
 
 describe('validation', () => {
   describe('#validationEmail', () => {
-    describe('emailが無効', () => {
-      test('空文字の場合エラーメッセージを返すこと', () => {
-        expect(validationEmail('')).toEqual('無効なメールアドレスです');
-      });
+    test('空文字の場合エラーメッセージを返すこと', () => {
+      expect(validationEmail('')).toEqual('無効なメールアドレスです');
+    });
 
-      test('@がない場合エラーメッセージを返すこと', () => {
-        expect(validationEmail('example.com')).toEqual(
-          '無効なメールアドレスです',
-        );
-      });
+    test('@がない場合エラーメッセージを返すこと', () => {
+      expect(validationEmail('example.com')).toEqual(
+        '無効なメールアドレスです',
+      );
     });
   });
 
   describe('#validationPassword', () => {
-    describe('passwordが無効', () => {
-      test('空文字の場合エラーメッセージを返すこと', () => {
-        expect(validationPassword('')).toEqual('入力必須項目です');
-      });
-      test('全角文字列の場合エラーメッセージを返すこと', () => {
-        expect(validationPassword('ほげほげほげほげ')).toEqual(
-          '英数字で入力してください',
-        );
-      });
-      test('6文字未満の場合エラーメッセージを返すこと', () => {
-        expect(validationPassword('eg34')).toEqual(
-          '6文字以上で入力してください',
-        );
-      });
+    test('空文字の場合エラーメッセージを返すこと', () => {
+      expect(validationPassword('')).toEqual('入力必須項目です');
+    });
+    test('全角文字列の場合エラーメッセージを返すこと', () => {
+      expect(validationPassword('ほげほげほげほげ')).toEqual(
+        '英数字で入力してください',
+      );
+    });
+    test('6文字未満の場合エラーメッセージを返すこと', () => {
+      expect(validationPassword('eg34')).toEqual('6文字以上で入力してください');
     });
   });
 });
