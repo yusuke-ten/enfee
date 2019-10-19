ab4528cf8653c691c2a8d70adf7ab18fce788022
aoba <ipbdipbd00@gmail.com>
2019-10-19 18:15:20 +0900

    Fix bug for cannot set token to localstorage when twitter login

--- src/containers/pages/LoginPage.tsx
+++ src/containers/pages/LoginPage.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { RootState } from 'src/modules';
-import { login } from 'modules/auth/actions';
 import Layout from 'components/Layout';
 import { LoginFormContainer } from 'containers/organisms';
 import { EntranceInfo } from 'components/organisms';
 import { EntranceTemplate } from 'components/templates';
+import { setToken } from 'utils/localStorage';
+import config from 'src/config';
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
 };
 
+// login処理が終わったらローカルストレージにtokenを一度保存して、location.hrefでメインページに遷移させている。
+// メインページにて、initailize処理にそのtokenを使用する。
+// この処理はtwitter, ID/PASS login 両方とも共通になっている。
+// 二度手間かもしれないが、initialize処理の流れが割とシンプルになるのでこうしている。(この辺りの処理はまだまだ模索中)
 const LoginPageContainer: React.FC = () => {
-  const { isLoggedIn } = useSelector((state: RootState) => state.auth);
-  const dispatch = useDispatch();
+  // twitterログインの場合: query parameterでtokenが渡されるためlocalstorageに保存してメインページに遷移
   const query = useQuery();
   const token = query.get('token');
-
-  if (isLoggedIn) {
-    return <Redirect to="/reviews/all" />;
+  if (token) {
+    setToken(token);
+    window.location.href = config.baseUrl;
   }
 
-  // twitterログインの場合にquery parameterでtokenが渡される
-  if (token) {
-    dispatch(login.succeed({ token }));
+  // 通常ログインの場合: isLoggedInがtrueになるので、メインページにリダイレクト
+  const { isLoggedIn } = useSelector((state: RootState) => state.auth);
+  if (isLoggedIn) {
+    window.location.href = config.baseUrl;
   }
 
   return (
