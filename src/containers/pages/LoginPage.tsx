961d50e4c08ef7fa12accd1b3cc59739b464b4c7
aoba <ipbdipbd00@gmail.com>
2019-10-21 20:37:49 +0900

    Fixed a bug where the login screen was displayed for a moment

--- src/containers/pages/LoginPage.tsx
+++ src/containers/pages/LoginPage.tsx
@@ -14,18 +14,23 @@ import config from 'src/config';
 // この処理はtwitter, ID/PASS login 両方とも共通になっている。
 // 二度手間かもしれないが、initialize処理の流れが割とシンプルになるのでこうしている。(この辺りの処理はまだまだ模索中)
 const LoginPageContainer: React.FC = () => {
-  // twitterログインの場合: query parameterでtokenが渡されるためlocalstorageに保存してメインページに遷移
   const query = useQuery();
+  const { isLoggedIn } = useSelector((state: RootState) => state.auth);
+
+  // twitterログインの場合: query parameterでtokenが渡されるためlocalstorageに保存してメインページに遷移
   const token = query.get('token');
   if (token) {
     setToken(token);
     window.location.href = config.baseUrl;
+
+    return null;
   }
 
   // 通常ログインの場合: isLoggedInがtrueになるので、メインページにリダイレクト
-  const { isLoggedIn } = useSelector((state: RootState) => state.auth);
   if (isLoggedIn) {
     window.location.href = config.baseUrl;
+
+    return null;
   }
 
   return (
